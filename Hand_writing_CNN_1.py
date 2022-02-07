6c910a0f6d6792f5cc220ec2e20bcbd57ab51e30
Andrey Lisitsa <75456617+And-Li@users.noreply.github.com>
2022-02-07 11:37:39 +0800

    The very beginning
    dataset and simplest dense layer model
    not working by the time:
    input data shape problem

---
+++ Hand_writing_CNN_1.py
@@ -0,0 +1,42 @@
+from tensorflow.keras.datasets import mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+from tensorflow.keras import utils
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from PIL import Image
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from tensorflow.keras.layers import Dropout, BatchNormalization
+from tensorflow.keras.optimizers import Adam
+
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
+
+y_train = utils.to_categorical(y_train, 10)
+y_test = utils.to_categorical(y_test, 10)
+
+print('x_train:', x_train.shape, type(x_train))
+print('x_test:', x_test.shape)
+print()
+print('y_train:', y_train.shape)
+print('y_test:', y_test.shape)
+
+
+model = Sequential()
+model.add(Dense(100, input_shape=x_train.shape, activation='relu'))
+model.add(Dense(1000, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',  # used for output of more than 2 classes ('sparce_CE' takes in class labels in 'discrete numbers' view, not in one-hot-encoding)
+              optimizer=Adam(learning_rate=0.001),
+              metrics=['accuracy'])
+model.fit(x_train,
+          y_train,
+          validation_split=0.2,
+          shuffle=True,
+          batch_size=128,
+          epochs=15,
+          verbose=1)
+
